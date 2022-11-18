a1e4a56cae555844538ef366d4bb91e611ede5f8
James Secor <jsecor@dealerpolicy.com>
2022-11-18 14:03:59 -0500

    homework done

--- src/components/nomophobia/School.tsx
+++ src/components/nomophobia/School.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useCallback, useMemo, useState } from 'react';
-import { InputGroup, Form, Button, Placeholder } from "react-bootstrap";
+import { InputGroup, Form, Button, Badge, Placeholder } from "react-bootstrap";
 import { BsFillBookmarkCheckFill, BsXCircleFill } from 'react-icons/bs';
 
 interface IFormAnswers {
@@ -9,6 +9,7 @@ interface IFormAnswers {
     2: number | null;
     3: number | null;
     4: number | null;
+    5: number | null;
 };
 
 interface ICheckedAnswers {
@@ -17,6 +18,7 @@ interface ICheckedAnswers {
     2: boolean | null;
     3: boolean | null;
     4: boolean | null;
+    5: boolean | null;
 };
 
 const getRandomZeroToNine = () => Math.round(Math.random() * 10) - 1;
@@ -29,7 +31,8 @@ const School = ({isLoading}) => {
         1: null,
         2: null,
         3: null,
-        4: null
+        4: null,
+        5: null
     });
 
     const [formAnswers, setFormAnswers] = useState<IFormAnswers>({
@@ -37,7 +40,8 @@ const School = ({isLoading}) => {
         1: null,
         2: null,
         3: null,
-        4: null
+        4: null,
+        5: null
     });
 
     const getProblemsAndAnswers = useCallback(() => {
@@ -65,50 +69,54 @@ const School = ({isLoading}) => {
             <Form onSubmit={(e) => submitForm(e)} className={'p-2 justify-content-center'}>
                 <Form.Label>Math Homework</Form.Label>
                 {allCorrect ? (
-                    <div>
-                        All good! You pass.
-                    </div>
-                ) : null}
-                {problemsAndAnswers.answers.map((answer, index) => {
-                    return (
-                        <InputGroup
-                            key={index}
-                            onChange={(e) => {
-                                const inputValue = (e.target as HTMLInputElement).value;
-                                setFormAnswers({...formAnswers, [index]: inputValue});
-                                setCheckedAnswers({
-                                    ...checkedAnswers,
-                                    [index]: (Number(answer) === Number(inputValue))
-                                });
-                            }}
-                            className={'my-2'}
+                    <>
+                        <Badge className={'position-absolute me-2 end-0'} pill={true} bg={'success'}>{'Done!'}</Badge>
+                        <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/episode/5lFTD6w4CxgyOxYsBaZ2nD?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </>
+                ) : (
+                    <>
+                        {problemsAndAnswers.answers.map((answer, index) => {
+                            return (
+                                <InputGroup
+                                    key={index}
+                                    onChange={(e) => {
+                                        const inputValue = (e.target as HTMLInputElement).value;
+                                        setFormAnswers({...formAnswers, [index]: inputValue});
+                                        setCheckedAnswers({
+                                            ...checkedAnswers,
+                                            [index]: (Number(answer) === Number(inputValue))
+                                        });
+                                    }}
+                                    className={'my-2'}
+                                >
+                                    <Button disabled={true} className={'p-0'} variant={'light'}>{`${index + 1}.`}</Button>
+                                        {isLoading ? (
+                                            <Placeholder as={InputGroup.Text} className={'w-50'} animation="glow">
+                                                <Placeholder className={'w-100'} />
+                                            </Placeholder>
+                                        ) : (
+                                            <InputGroup.Text id="basic-addon3">
+                                                {`${problemsAndAnswers.x[index]} x ${problemsAndAnswers.y[index]} = `}
+                                            </InputGroup.Text>
+                                        )}
+                                    <Form.Control autoComplete={'off'} type={'number'} id={`question-${index}`} aria-describedby="basic-addon3" />
+                                    <div className={'position-absolute end-0 me-1'}>
+                                        {checkedAnswers[index] 
+                                            ? <BsFillBookmarkCheckFill color={'green'} /> 
+                                            : (checkedAnswers[index] === false ? <BsXCircleFill color={'orangered'} /> : null)}
+                                    </div>                                
+                                </InputGroup>
+                            )
+                        })}
+                        <Button
+                            disabled={completed}
+                            className={`w-100 ${completed ? 'btn-success' : ''}`}
+                            type={'submit'}
                         >
-                            <Button disabled={true} className={'p-0'} variant={'light'}>{`${index + 1}.`}</Button>
-                                {isLoading ? (
-                                    <Placeholder as={InputGroup.Text} className={'w-50'} animation="glow">
-                                        <Placeholder className={'w-100'} />
-                                    </Placeholder>
-                                ) : (
-                                    <InputGroup.Text id="basic-addon3">
-                                        {`${problemsAndAnswers.x[index]} x ${problemsAndAnswers.y[index]} = `}
-                                    </InputGroup.Text>
-                                )}
-                            <Form.Control autoComplete={'off'} type={'number'} id={`question-${index}`} aria-describedby="basic-addon3" />
-                            { completed && (
-                                <div className={'position-absolute end-0 me-1'}>
-                                    {checkedAnswers[index] ? <BsFillBookmarkCheckFill color={'green'} /> : <BsXCircleFill color={'orangered'} />}
-                                </div>
-                            )}
-                        </InputGroup>
-                    )
-                })}
-                <Button
-                    disabled={completed}
-                    className={`w-100 ${completed ? 'btn-success' : ''}`}
-                    type={'submit'}
-                >
-                    {completed ? 'Complete!' : 'Submit Worksheet'}
-                </Button>
+                            {completed ? 'Complete!' : 'Submit Worksheet'}
+                        </Button>
+                    </>
+                )}
             </Form>
         </div>
     );
