d496d3f525a54cb1e86f8bd22a5d03049504b5f3
James Secor <james.secor@gmail.com>
2022-11-21 22:00:07 -0500

    formatting school instr

--- src/components/nomophobia/SchoolInstructions.tsx
+++ src/components/nomophobia/SchoolInstructions.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Toast, Button } from 'react-bootstrap';
 import { Pages } from './Nomophobia';
 
-const INSTRUCTIONS_1 = 'During the pandemic many, if not all, Americans were forced to do their learning remotely. Due to varying levels of internet access, some could not finish their work on time.';
+const INSTRUCTIONS_1 = 'During the pandemic many, if not all, Americans in school were forced to do their learning remotely. Due to disparities in internet access, some were unable to keep up with their work.';
 const INSTRUCTIONS_2 = 'Can you finish this worksheet before the it\'s due? Watch out for buffering!';
 
 const SchoolInstructions = ({ isLoading, setCurrentPage }) => {
@@ -14,11 +14,12 @@ const SchoolInstructions = ({ isLoading, setCurrentPage }) => {
 
     return (
         <Toast>
-            <Toast.Header>
+            <Toast.Header className={'justify-content-between'}>
                 {'Homework game'}
             </Toast.Header>
             <Toast.Body>
                 {INSTRUCTIONS_1}
+                <br />
                 <br />
                 {INSTRUCTIONS_2}
                 {isLoadingHomework ? (
