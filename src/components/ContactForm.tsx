2c66aaec3c14502c768c648a1424f5a1cbc07ec7
James Secor <james.secor@gmail.com>
2023-02-04 17:31:37 -0500

    use new api for email

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { useRef, useState } from "react";
-import { Container, Button, Row, Col, Toast } from 'react-bootstrap';
+import { Container, Button, Row, Col, Toast, Spinner } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
-import emailjs from '@emailjs/browser';
 import { useNavigate } from "react-router-dom";
 import { BackgroundColorContext, textColor } from "./BackgroundColorProvider";
 
@@ -13,6 +12,7 @@ const ContactForm = () => {
     const [message, setMessage] = useState('');
     const formRef = useRef<HTMLFormElement>(null);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isSuccessful, setIsSuccessful] = useState(false);
     const navigateTo = useNavigate();
 
     const handleChange = (e) => {
@@ -22,15 +22,25 @@ const ContactForm = () => {
         if (e.target.id === "message") setMessage(e.target.value);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setIsSubmitted(true);
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID ?? '', process.env.REACT_APP_TEMPLATE_ID ?? '', formRef.current ?? '', process.env.REACT_APP_PUBLIC_KEY ?? '')
-            .then((result) => {
-                // console.log(result.text);
-            }, (error) => {
-                // console.log(error.text);
-            });
+
+        const emailResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/email`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                message: message
+            })
+        });
+        if (emailResponse) {
+            setIsSuccessful(true);
+        }
     }
 
     return (
@@ -40,7 +50,7 @@ const ContactForm = () => {
                     <Row xs={1}>
                         <Col>
                             {
-                                isSubmitted
+                                isSuccessful
                                     ? (
                                         <Toast
 
@@ -52,9 +62,9 @@ const ContactForm = () => {
                                                 <strong className="me-auto">{'Success!'}</strong>
                                             </Toast.Header>
                                             <Toast.Body>
-                                                {'You\'re message has been sent directly to, and only to, James.'}
-                                                <br /><br />
                                                 {'Thanks for reaching out!'}
+                                                <br /><br />
+                                                {'You\'re message has been sent to James.'}
                                             </Toast.Body>
                                         </Toast>
                                     )
@@ -63,21 +73,26 @@ const ContactForm = () => {
                                             <h5 className="pb-4 d-flex justify-content-center">join email list / leave a message</h5>
                                             <Form.Group className="mb-3" controlId="firstname">
                                                 <Form.Label>First Name</Form.Label>
-                                                <Form.Control name='firstname' type="text" value={firstname} onChange={handleChange} />
+                                                <Form.Control disabled={isSubmitted} name='firstname' type="text" value={firstname} onChange={handleChange} />
                                             </Form.Group>
                                             <Form.Group className="mb-3" controlId="lastname">
                                                 <Form.Label>Last Name</Form.Label>
-                                                <Form.Control name='lastname' type="text" value={lastname} onChange={handleChange} />
+                                                <Form.Control disabled={isSubmitted} name='lastname' type="text" value={lastname} onChange={handleChange} />
                                             </Form.Group>
                                             <Form.Group className="mb-3" controlId="email">
                                                 <Form.Label>Email</Form.Label>
-                                                <Form.Control required name='email' type="email" value={email} onChange={handleChange} />
+                                                <Form.Control disabled={isSubmitted} required name='email' type="email" value={email} onChange={handleChange} />
                                             </Form.Group>
                                             <Form.Group className="mb-3" controlId="message">
                                                 <Form.Label>Message</Form.Label>
-                                                <Form.Control rows={3} as="textarea" name='message' value={message} onChange={handleChange} />
+                                                <Form.Control disabled={isSubmitted} rows={3} as="textarea" name='message' value={message} onChange={handleChange} />
                                             </Form.Group>
-                                            <Button className={'w-100'} type='submit' variant={'success'} disabled={isSubmitted}>Submit</Button>
+                                            <Button className={'w-100'} type='submit' variant={'success'} disabled={isSubmitted}>
+                                                {isSubmitted
+                                                    ? <Spinner variant={'info'} animation={'border'} />
+                                                    : <span>Submit</span>
+                                                }
+                                            </Button>
                                         </Form>
                                     )
                             }
